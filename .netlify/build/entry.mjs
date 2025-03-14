5e99147d1e24b56ca65f58ad25e090d0f6cb31df
Sergio Saidel Mancinas Sarabia <saidels1999@outlook.com>
2025-03-14 15:54:36 -0600

    Update Netlify build entry and manifest files with new middleware secret and add manifest implementation

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CP4KloTQ.mjs';
+import { manifest } from './manifest_BCILAthg.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "b85870a6-0a75-4cd1-b0c2-94dd3d3bd3a1"
+    "middlewareSecret": "65efcd5e-d731-435e-ad32-941f687ef360"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
