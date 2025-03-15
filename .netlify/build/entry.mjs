8468e9b4703bc0061c4e60b0c779bd00143604b3
Sergio Saidel Mancinas Sarabia <saidels1999@outlook.com>
2025-03-15 17:19:13 -0600

    Agregar archivos de configuración para Netlify y mejorar la animación del título en el componente héroe

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,31 @@
+import { renderers } from './renderers.mjs';
+import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_DtT1SFpq.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/index.astro", _page0]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "217b71dc-0ef0-453d-bea7-e60d45783170"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
