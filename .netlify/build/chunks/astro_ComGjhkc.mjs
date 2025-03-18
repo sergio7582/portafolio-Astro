e1cf2d8c48c1e2964fe9fa80a8592cb8f96250e7
Sergio Saidel Mancinas Sarabia <saidels1999@outlook.com>
2025-03-17 22:29:21 -0600

    Eliminar archivos innecesarios de prerenderizado y ajustar la estructura de componentes

--- .netlify/build/chunks/astro_ComGjhkc.mjs
+++
@@ -1,4 +0,0 @@
-import 'kleur/colors';
-import 'html-escaper';
-import 'clsx';
-import './astro/server_B9tfJLiE.mjs';
--- .netlify/build/pages/index.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/index_BBP4HKe5.mjs';
-export { renderers } from '../renderers.mjs';
